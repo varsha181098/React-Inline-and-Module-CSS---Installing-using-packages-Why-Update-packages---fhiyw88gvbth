0ed42190a19a24d3ae97fc6bb3a3e88c527d79e7
varsha181098 <118925989+varsha181098@users.noreply.github.com>
2023-01-03 23:04:21 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,13 @@ import React from 'react'
 import '../styles/App.css';
 import styles from '../styles/appStyles.module.css';
 const App = () => {
-const heading ={
-
-
-}
+  const heading = 'React Inline and Module CSS';
+  
   return (
     <div id="main">
-      <h1 >Success</h1>
-      <h1 id='error' >Error</h1>
+      <h1 className='heading'>{heading}</h1>
+      <h1 className={styles.success} >Success</h1>
+      <h1 id='error' style={{color:'red', fontSize:"50px"}}>Error</h1>
     </div>
   )
 }
